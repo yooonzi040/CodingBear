21739c448bc446596e6e3e5cf0d6023374fbf3f9
yoooonji040 <yoouuii22@naver.com>
2023-11-15 16:02:21 +0900

    First Commit

---
+++ app/src/main/java/sku/jyj/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package sku.jyj.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
