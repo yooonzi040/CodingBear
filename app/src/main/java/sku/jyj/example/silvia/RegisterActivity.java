1c2e478ff7aea92267edb39b8af1298dabd6ef8e
zhurong5 <ggrong567@gmail.com>
2024-03-09 16:47:46 +0900

    회원가입 기입 정보 수정

--- app/src/main/java/sku/jyj/example/silvia/RegisterActivity.java
+++ app/src/main/java/sku/jyj/example/silvia/RegisterActivity.java
@@ -33,7 +33,8 @@ public class RegisterActivity extends AppCompatActivity {
     // Flask 서버 호출
     private static final String urls = "http://192.168.0.4:5000/userJoin";
     public Button btn_birth;
-    private EditText input_id, input_pw, input_name, input_age;
+    private String dateMessage;
+    private EditText input_id, input_pw, input_name, input_phoneno;
     private Button bt_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) { // 액티비티 시작시 처음으로 실행되는 생명주기
@@ -44,20 +45,20 @@ public class RegisterActivity extends AppCompatActivity {
         input_id = findViewById(R.id.input_id);
         input_pw = findViewById(R.id.input_pw);
         input_name = findViewById(R.id.input_name);
-        input_age = findViewById(R.id.input_age);
+        input_phoneno = findViewById(R.id.input_phoneno);
         btn_birth = findViewById(R.id.btn_birth);
         bt_register = findViewById(R.id.bt_register);
 
-        // 나이 입력값 최대 길이 제한
-        input_age.addTextChangedListener(new TextWatcher() {
+        // 전화번호 입력값 최대 길이 제한
+        input_phoneno.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 InputFilter[] filters = new InputFilter[1];
-                filters[0] = new InputFilter.LengthFilter(3); // et_age의 입력 최대길이 제한
-                input_age.setFilters(filters);
+                filters[0] = new InputFilter.LengthFilter(11); // et_age의 입력 최대길이 제한
+                input_phoneno.setFilters(filters);
             }
 
             @Override
@@ -65,13 +66,29 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    public void processDatePickerResult(int year, int month, int day){
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        dateMessage = (month_string + "/" + day_string + "/" + year_string);
+
+        Toast.makeText(this,"Date: "+dateMessage,Toast.LENGTH_SHORT).show();
+
+        sendServer(dateMessage);
+    }
+
+    public void onBitrhdayClicked (View view) {
+        DatePickerFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
     public void onRegisterClicked(View view){ // 버튼 클릭 리스너
         // 버튼 클릭 시 서버로 데이터 전송
         Log.d("ClickButton1", "Button clicked");  // Log 추가
-        sendServer();
+        sendServer(dateMessage);
     }
 
-    public void sendServer(){ // 서버로 데이터 전송하기 위한 함수
+    public void sendServer(final String dateMessage){ // 서버로 데이터 전송하기 위한 함수
         class sendData extends AsyncTask<Void, Void, String> { // 백그라운드 쓰레드 생성
 
             @Override
@@ -113,9 +130,12 @@ public class RegisterActivity extends AppCompatActivity {
                     OkHttpClient client = new OkHttpClient(); // OkHttp를 사용하도록 OkHttpClient 객체를 생성
 
                     JSONObject jsonInput = new JSONObject();  // JSON 객체 생성
-                    jsonInput.put("userID",  input_id.getText().toString()); // [vscode-app.py] JSON 객체에 데이터 추가 (id)
-                    jsonInput.put("userPassword",  input_pw.getText().toString()); // [vscode-app.py] JSON 객체에 데이터 추가 (pw)
-                    jsonInput.put("userName", input_name.getText().toString());  // [vscode-app.py] JSON 객체에 데이터 추가 (nickname)
+
+                    jsonInput.put("userID", input_id.getText().toString()); // [vscode-app.py] JSON 객체에 데이터 추가 (id)
+                    jsonInput.put("userPassword", input_pw.getText().toString()); // [vscode-app.py] JSON 객체에 데이터 추가 (pw)
+                    jsonInput.put("userName", input_name.getText().toString()); // [vscode-app.py] JSON 객체에 데이터 추가 (name)
+                    jsonInput.put("userPhoneno", input_phoneno.getText().toString()); // [vscode-app.py] JSON 객체에 데이터 추가 (phoneno)
+                    jsonInput.put("userBirth", dateMessage);
 
                     RequestBody reqBody = RequestBody.create(
                             jsonInput.toString(),
@@ -142,19 +162,5 @@ public class RegisterActivity extends AppCompatActivity {
         // AsyncTask 실행
         sendData sendData = new sendData();
         sendData.execute(); // 웹 서버에 데이터 전송
-    }
-
-    public void processDatePickerResult(int year, int month, int day){
-        String month_string = Integer.toString(month+1);
-        String day_string = Integer.toString(day);
-        String year_string = Integer.toString(year);
-        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
-
-        Toast.makeText(this,"Date: "+dateMessage,Toast.LENGTH_SHORT).show();
-    }
-
-    public void onBitrhdayClicked (View view) {
-        DatePickerFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
